417f1e220e861ec485c0f4affc6e7f717f24fae4
Little Robot Games <76882552+littlerobotgames@users.noreply.github.com>
2025-11-01 11:56:14 -0600

    Initial Commit

---
+++ Maze/Program.cs
@@ -0,0 +1,9 @@
+﻿//Alex Gardner 11/01/25 Lab 9: Maze
+//This program will create a simple maze for the user to navigate through in the console.
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
